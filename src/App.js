47ed997e0cd790c1936d665fd6895d8c54b91ca9
Maul <mprambadi@gmail.com>
2018-11-21 04:40:21 +0700

    finish list

--- src/App.js
+++ src/App.js
@@ -1,14 +1,58 @@
-import React, { Component } from 'react';
-import user from  './user'
-
+import React, { Component } from "react";
+import users from "./user";
+import { hot } from "react-hot-loader";
+import "./App.scss";
 class App extends Component {
+  state = {
+    search: ""
+  };
+
+  handleSearch = e => {
+    this.setState({ search: e.target.value });
+  };
   render() {
+    const { search } = this.state;
     return (
-      <div className="App">
-        
+      <div className="app">
+        <div className="content">
+          <input
+            placeholder="search user"
+            value={search}
+            onChange={this.handleSearch}
+            className="search"
+          />
+          <UserList users={users} search={search} />
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+const UserList = ({ users, search }) => {
+  const filter = users.filter(
+    user =>
+      [user.name, user.username, user.email]
+        .join(" ")
+        .toLowerCase()
+        .indexOf(search) !== -1
+  );
+
+  return (
+    <div >
+        <div className="user-header">
+            <div>Name</div>
+            <div>Username</div>
+            <div>Email</div>
+        </div>
+      {filter.map(user => (
+        <div key={user.id} className="user-list">
+          <div>{user.name}</div>
+          <div>{user.email}</div>
+          <div>{user.name}</div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default hot(module)(App);
